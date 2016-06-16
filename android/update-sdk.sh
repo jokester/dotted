2b4052deb8b185d639ccccb8ef04590c80139f61
Wang Guan <momocraft@gmail.com>
2016-06-16 22:25:08 +0900

    download android sdk

--- android/update-sdk.sh
+++ android/update-sdk.sh
@@ -10,6 +10,29 @@ else
   exit 1
 fi
 
-# --filter: Accepted values are: [add-on, doc, extra, platform, platform-tool, sample, source, system-image, tool]
-exec tools/android update sdk --no-ui --all --filter extra,platform,platform-tool,tool,add-on
+android () {
+  "$(dirname $0)/tools/android" "$@"
+}
 
+function install_sdk {
+  android update sdk -u -s -a -t "$1"
+}
+
+function fetch_package_indices {
+  android list sdk -u -s -a         \
+    | grep -iv obsolete             \
+    | grep -iv sources              \
+    | grep -iv 'system image'       \
+    | sed 's/^[ ]*\([0-9]*\).*/\1/' \
+    | grep -v '^\s*$'               \
+    | paste -d, -s -
+}
+
+main () {
+  (while : ; do
+  echo 'y'
+  sleep 1
+  done) | android update sdk -u -s -a -t "$(fetch_package_indices)"
+}
+
+main
