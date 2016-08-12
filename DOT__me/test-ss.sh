0de953f3dee9ca942dbf19c010acb469a1537f3a
Wang Guan <momocraft@gmail.com>
2016-08-13 07:03:24 +0900

    a script to test ss server

---
+++ DOT__me/test-ss.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -ue
+
+URL="http://ipinfo.io"
+
+log () {
+  echo $(date) "$@"
+}
+
+info () {
+  log 'INFO' "$@"
+}
+
+error () {
+  log 'ERROR' "$@"
+}
+
+LOCAL_PORT=11080
+
+test-server () {
+  local ENC_METHOD="$1"
+  local PASSWORD="$2"
+  local SERVER_PORT="$3"
+  local SERVER_ADDR="$4"
+
+  info "starting ss-local for server $SERVER_ADDR"
+
+  ss-local \
+    -s "$SERVER_ADDR" \
+    -p "$SERVER_PORT" \
+    -m "$ENC_METHOD"  \
+    -i "127.0.0.1"    \
+    -k "$PASSWORD"    \
+    -l $LOCAL_PORT    \
+    -v &>"/tmp/test-ss.$$.log" &
+
+  local SS_CLIENT_PID=$!
+  local LOGFILE="/tmp/test-ss.$SS_CLIENT_PID.log"
+  sleep 1
+  if ! ps $SS_CLIENT_PID &>/dev/null; then
+    error "could not start ss-local. check $LOGFILE for log"
+    exit 2
+  else
+    info "started ss-local. see $LOGFILE for verbose log"
+  fi
+
+  sleep 5
+  if curl ipinfo.io --proxy "socks5://127.0.0.1:$LOCAL_PORT" --max-time 10 ; then
+    echo
+    info "server at $SERVER_ADDR:$SERVER_PORT is working normally"
+  else
+    echo
+    info "server at $SERVER_ADDR:$SERVER_PORT does not work"
+  fi
+
+  kill -9 $SS_CLIENT_PID
+  wait &>/dev/null
+
+  echo
+  echo
+}
+
+if [[ $# -lt 4 ]]; then
+  echo "USAGE: $0 ENC_METHOD PASSWORD SERVER_PORT [SERVER_ADDR]+"
+  exit 1
+fi
+
+ENC_METHOD="$1"
+PASSWORD="$2"
+SERVER_PORT="$3"
+while [[ $# -ge 4 ]]; do
+  test-server "$ENC_METHOD" "$PASSWORD" "$SERVER_PORT" "$4"
+  shift
+  LOCAL_PORT=$(( 1+LOCAL_PORT ))
+done
+
